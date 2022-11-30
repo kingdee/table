ff0c08d2163e6502b8514d0262bbf6e41bedb56a
wqhui <qinghui_wu@foxmail.com>
2022-11-30 09:45:43 +0800

    refactor: 修改过滤默认图标&调整过滤图标弹出位置保持弹出面板图标与列头图标对齐

--- components/table/pipeline/features/filter/FilterPanel.tsx
+++ components/table/pipeline/features/filter/FilterPanel.tsx
@@ -1,6 +1,7 @@
 import React, { CSSProperties, ReactNode, useEffect, useState, useRef } from 'react'
 import styled from 'styled-components'
 import { isElementInEventPath, keepWithinBounds } from '../../../utils/'
+import DefaultFilterIcon from './DefaultFilterIcon'
 
 const FilterPanelStyle = styled.div`
   display: flex;
@@ -17,10 +18,10 @@ const FilterPanelStyle = styled.div`
     background-color: #ebedf1;
 
     .popup-header-icon {
+      display: flex;
       color:#666;
       background-color: #fff;
-      padding: 6px 16px 6px 16px;
-      display: inline-block;
+      padding: 8px 16px 8px 16px;
       border-right: 1px solid transparent;
       border-left: 1px solid transparent;
       border-top: 1px solid transparent;
@@ -96,21 +97,7 @@ function FilterPanel ({ style, children, position, filterIcon, onClose }) {
       <div className={'popup-header'}>
         <span className={'popup-header-icon'}>
           {
-            filterIcon || <svg
-              width={14}
-              height={14}
-              viewBox="64 64 896 896"
-              focusable="false"
-              data-icon="filter"
-              fill="currentColor"
-              aria-hidden="true"
-            >
-              <path
-                d="M349 838c0 17.7 14.2 32 31.8 32h262.4c17.6 0 31.8-14.3
-              31.8-32V642H349v196zm531.1-684H143.9c-24.5 0-39.8 26.7-27.5
-              48l221.3 376h348.8l221.3-376c12.1-21.3-3.2-48-27.7-48z"
-              ></path>
-            </svg>
+            filterIcon || <DefaultFilterIcon width={12} height={12} />
           }
         </span>
       </div>
