0f118d993cdab77fb6d5d7e2c47e73a80090fca2
haohao_peng <haohao_peng@kingdee.com>
2024-09-18 15:26:52 +0800

    fix: [cli] 修复build后的文件中含有注释的问题 fix #99

--- babel.config.js
+++ babel.config.js
@@ -6,17 +6,21 @@ module.exports = {
       {
         corejs: 3,
         useBuiltIns: 'usage'
-      },
+      }
     ],
     '@babel/preset-react',
-    '@babel/preset-typescript',
+    '@babel/preset-typescript'
   ],
   plugins: [
-    [
-      '@babel/plugin-transform-runtime',
-    ],
+    ['@babel/plugin-transform-runtime'],
     '@babel/plugin-transform-object-assign',
     '@babel/plugin-proposal-export-namespace-from',
-    '@babel/plugin-proposal-class-properties'
-  ],
+    '@babel/plugin-proposal-class-properties',
+    [
+      'babel-plugin-styled-components',
+      {
+        minify: true
+      }
+    ]
+  ]
 }
