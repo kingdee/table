4fd2cc897d897d01c8b9a950cee1acb7c6e6dd11
潘小安 <panan521@qq.com>
2025-07-29 14:52:54 +0800

    fix: [bos][PRJ-00623156][-] 滚动之外的其他数据更新，也要实施更新滚动条位置

--- components/table/base/BlankComponent.tsx
+++ components/table/base/BlankComponent.tsx
@@ -1,17 +1,19 @@
-import React, { useState, useImperativeHandle, forwardRef, memo } from 'react'
+import React, { useState, useImperativeHandle, forwardRef, memo, useEffect } from 'react'
 import cx from 'classnames'
 import { Classes } from './styles'
 
 interface BlankProps {
   height: number
-  className?: string
 }
 
 export interface BlankRef {
   updateHeight: (newHeight: number) => void
 }
-const TopBlankComponent = forwardRef<BlankRef, BlankProps>(({ height: initialHeight }, ref) => {
-  const [height, setHeight] = useState(initialHeight)
+const TopBlankComponent = forwardRef<BlankRef, BlankProps>(({ height: heightFromProps }, ref) => {
+  useEffect(() => {
+    setHeight(heightFromProps)
+  }, [heightFromProps])
+  const [height, setHeight] = useState(heightFromProps)
   useImperativeHandle(ref, () => ({
     updateHeight: (newHeight: number) => {
       if (height !== newHeight) {
@@ -20,13 +22,22 @@ const TopBlankComponent = forwardRef<BlankRef, BlankProps>(({ height: initialHei
     }
   }), [height])
 
-  return <div style={{ height }} />
+  return (
+    <div
+      key="top-blank"
+      className={cx(Classes.virtualBlank, 'bottom')}
+      style={{ height }}
+    />
+  )
 })
 
 TopBlankComponent.displayName = 'TopBlank'
 
-const BottomBlankComponent = forwardRef<BlankRef, BlankProps>(({ height: initialHeight, className }, ref) => {
-  const [height, setHeight] = useState(initialHeight)
+const BottomBlankComponent = forwardRef<BlankRef, BlankProps>(({ height: heightFromProps }, ref) => {
+  const [height, setHeight] = useState(heightFromProps)
+  useEffect(() => {
+    setHeight(heightFromProps)
+  }, [heightFromProps])
   useImperativeHandle(ref, () => ({
     updateHeight: (newHeight: number) => {
       if (height !== newHeight) {
@@ -42,7 +53,7 @@ const BottomBlankComponent = forwardRef<BlankRef, BlankProps>(({ height: initial
   return (
     <div
       key="bottom-blank"
-      className={cx(Classes.virtualBlank, 'bottom', className)}
+      className={cx(Classes.virtualBlank, 'bottom')}
       style={{ height }}
     />
   )
@@ -52,9 +63,9 @@ BottomBlankComponent.displayName = 'BottomBlank'
 
 // 使用 memo 优化，只有当 height 或 className 改变时才重新渲染
 export const TopBlank = memo(TopBlankComponent, (prevProps, nextProps) => {
-  return prevProps.height === nextProps.height && prevProps.className === nextProps.className
+  return prevProps.height === nextProps.height
 })
 
 export const BottomBlank = memo(BottomBlankComponent, (prevProps, nextProps) => {
-  return prevProps.height === nextProps.height && prevProps.className === nextProps.className
+  return prevProps.height === nextProps.height
 })
